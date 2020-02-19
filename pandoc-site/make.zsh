04e0c0f43166f4bdd8d4d513babb1bcdfe7f3de7
Praveen Batra <praveen.batra@berkeley.edu>
2020-02-18 18:06:45 -0800

    New website

---
+++ pandoc-site/make.zsh
@@ -0,0 +1,31 @@
+#!/usr/bin/env zsh
+
+echo "Removing old files..."
+rm -v ../docs/*.html
+rm -v temp/*.temp
+
+echo "Compiling links..."
+echo "<span class=\"toc\">" >> temp/links.temp
+for infile in sources/*.md; do
+    infile=$(basename $infile)
+    file=${infile%.*}
+    outfile=${file}.html
+    echo "<a href=\"${outfile}\" class=\"toc_entry\">${file}</a><br />" >> temp/links.temp
+done
+echo "</span><hr>" >> temp/links.temp
+
+cat t0.html temp/links.temp t1.html > temp/template.temp
+
+echo "Generating outputs..."
+mkdir -p ../docs
+for infile in sources/*.md; do
+    infile=$(basename $infile)
+    file=${infile%.*}
+    outfile=${file}.html
+    pandoc -o ../docs/$outfile sources/$infile --standalone --standalone --mathml --template temp/template.temp --metadata pagetitle=$file -c resources/styles.css
+done
+
+echo "Copying resources..."
+cp -r sources/resources ../docs
+
+echo "Make complete!"
---
+++ pandoc-site/sources/index.md
@@ -0,0 +1,15 @@
+# Looking for [Orchestrator site](http://18praveenb.github.io/orchestrator) or [CSUA guide](http://18praveenb.github.io/orchestrator/csua.html)?
+
+## Praveen Batra
+
+![](resources/me.jpg){width=120px height=120px .circled}
+
+I am an undergraduate at UC Berkeley studying to receive a Bachelors of Science in Electrical Engineering and Computer Science in May 2022.
+
+My interests are in machine learning and software development.
+
+- [LinkedIn](https://www.linkedin.com/in/18-praveen-batra/)
+- [GitHub](https://github.com/18praveenb/)
+- [Twitter](https://twitter.com/18praveenb)
+- [Blog](https://skuserblognode.blogspot.com)
+- [SoundCloud](https://soundcloud.com/integer-literal)
