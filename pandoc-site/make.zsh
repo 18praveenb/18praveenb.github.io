1caed74e931e5bcdb6badc7b9d0771e81d31eb29
Praveen Batra <praveen.batra@berkeley.edu>
2020-03-02 01:09:03 -0800

    Remove index from nav bar

--- pandoc-site/make.zsh
+++ pandoc-site/make.zsh
@@ -1,4 +1,5 @@
 #!/usr/bin/env zsh
+setopt null_glob
 
 echo "Removing old files..."
 rm -v ../*.html
@@ -6,22 +7,22 @@ rm -v temp/*.temp
 
 echo "Compiling links..."
 echo "<span class=\"toc\">" >> temp/links.temp
-for infile in sources/*.md; do
+for infile in sources/[^_]*.md; do
     infile=$(basename $infile)
     file=${infile%.*}
     outfile=${file}.html
-    echo "<a href=\"${outfile}\" class=\"toc_entry\">${file}</a><br />" >> temp/links.temp
+    echo "<a href=\"./${outfile}\" class=\"toc_entry\">${file}</a>&nbsp;&nbsp;&nbsp;&nbsp;" >> temp/links.temp
 done
-echo "</span><hr>" >> temp/links.temp
+echo "</span>" >> temp/links.temp
 
 cat t0.html temp/links.temp t1.html > temp/template.temp
 
 echo "Generating outputs..."
-for infile in sources/*.md; do
+for infile in sources/*.md*; do
     infile=$(basename $infile)
     file=${infile%.*}
-    outfile=${file}.html
-    pandoc -o ../$outfile sources/$infile --standalone --standalone --mathml --template temp/template.temp --metadata pagetitle=$file -c resources/styles.css
+    outfile=${file#_}.html
+    pandoc -o ../$outfile sources/$infile --standalone --standalone --mathml --template temp/template.temp --metadata pagetitle=${file#_} -c resources/styles.css
 done
 
 echo "Copying resources..."
